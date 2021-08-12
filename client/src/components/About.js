673fc50984e9bd0ad1cbbfc290663a9c2b873dd5
Priyanka Vishwakarma <72207846+Its-Priyanka@users.noreply.github.com>
2021-08-12 22:16:05 +0530

    Add files via upload

---
+++ client/src/components/About.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import dp from '../images/index.jpg';
+import meme from '../images/man.jpg';
+import edit from '../images/5465.jpg';
+function About() {
+    return (
+        <div>
+          <h2>About Me</h2>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+	</ol>   
+
+	<div class="carousel-inner">		
+		<div class="carousel-item active">
+			<div class="img-box"><img src={dp} alt=""/></div>
+			<p class="testimonial"></p>
+			<p class="overview"><b></b>MERN stack developer  <a href="#"></a></p>
+			<div class="star-rating">
+				<ul class="list-inline">
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+				</ul>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<div class="img-box"><img src={edit} alt=""/></div>
+			<p class="testimonial">I am intrested in MERN stack development.I have worked in technologies like Reactjs ,Nodejs,Express js,Mongo DB</p>
+			<p class="overview"><b>Priyanka Vishwakarma</b>MERN stack developer   <a href="#"></a></p>
+			<div class="star-rating">
+				<ul class="list-inline">
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+				</ul>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<div class="img-box"><img src={meme} alt=""/></div>
+			<p class="testimonial"> Final Year student Of Banaras Hindu University</p>
+			<p class="overview"><b>Priyanka Vishwakarma</b>Web Developer  <a href="#"></a></p>
+			<div class="star-rating">
+				<ul class="list-inline">
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star-half-o"></i></li>
+				</ul>
+			</div>
+		</div>		
+	</div>
+
+	<a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+		<i class="fa fa-angle-left"></i>
+	</a>
+	<a class="carousel-control-next" href="#myCarousel" data-slide="next">
+		<i class="fa fa-angle-right"></i>
+	</a>
+</div>
+        </div>
+    )
+}
+
+export default About
